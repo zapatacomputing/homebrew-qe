73abe5e0dca3fc151db750a1fce9f0911eedd76c
Ntwali B <toussaint@zapatacomputing.com>
2020-03-13 09:36:54 +0100

    Initial commit of QE brew formula.

---
+++ qe.rb
@@ -0,0 +1,13 @@
+class QE < Formula
+    desc "Quantum Engine command line tool for running workflows"
+    homepage "https://orquestra.io/docs/"
+    url "https://github.com/zapatacomputing/qe-public/raw/master/archives/qe-0.0.1.tar.gz"
+    sha256 "e5fc6ee5d356d2738412a12a0b3ebc453c2b36a7d93193e512b92bfdc5757a10"
+    version "0.0.1"
+  
+    bottle :unneeded
+  
+    def install
+      bin.install "qe"
+    end
+  end
