f9832523634a529b6f4606650ef30fe7a19c5bf5
Ntwali B <toussaint@zapatacomputing.com>
2020-03-13 09:48:53 +0100

    Updated sha of binary.

--- qe.rb
+++ qe.rb
@@ -2,7 +2,7 @@ class Qe < Formula
     desc "Quantum Engine command line tool for running workflows"
     homepage "https://orquestra.io/docs/"
     url "https://github.com/zapatacomputing/qe-public/raw/master/archives/qe-0.0.1.tar.gz"
-    sha256 "e5fc6ee5d356d2738412a12a0b3ebc453c2b36a7d93193e512b92bfdc5757a10"
+    sha256 "02f1eb357c7d57da6c38f38650b337323c5026f48b38dd8aef8cf6b5a55bbd28"
     version "0.0.1"
   
     bottle :unneeded
